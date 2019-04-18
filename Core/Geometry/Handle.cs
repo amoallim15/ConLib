92046c912c6f99f72365afa5d1cea84bc4b8a798
Ali Moallim <axj.159@gmail.com>
2019-04-18 19:21:01 +0900

    refactoring

---
+++ Core/Geometry/Handle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConLib.Core.Geometry
+{
+    public class Handle
+    {
+        public int Id { get; set; }
+        public Handle(int id = -1)
+        {
+            Id = id;
+        }
+        public bool IsValid()
+        {
+            return Id > 0;
+        }
+        public void Reset()
+        {
+            Id = -1;
+        }
+        public static bool operator ==(Handle lhs, Handle rhs)
+        {
+            return lhs.Id == rhs.Id;
+        }
+        public static bool operator !=(Handle lhs, Handle rhs)
+        {
+            return lhs.Id != rhs.Id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Id == ((Handle)obj).Id;
+        }
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+        protected void Increment()
+        {
+            Id++;
+        }
+        protected void Decrement()
+        {
+            Id--;
+        }
+        protected void Increment(int amount)
+        {
+            Id += amount;
+        }
+        protected void Decrement(int amount)
+        {
+            Id -= amount;
+        }
+    }
+}
---
+++ Core/Geometry/Items.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConLib.Core.Geometry
+{
+    public struct Vertex
+    {
+        public Handle HalfEdge { get; set; }
+    }
+
+    public struct Face
+    {
+        public Handle HalfEdge { get; set; }
+    }
+
+    public struct HalfEdge
+    {
+        public Handle Opposite { get; set; }
+        public Handle Next { get; set; }
+        public Handle Prev { get; set; }
+        public Face Face { get; set; }
+        public Vertex Vertex { get; set; }
+    }
+
+    public struct HalfEdgeDS
+    {
+        public List<Vertex> Vertices { get; set; }
+        public List<HalfEdge> HalfEdges { get; set; }
+        public List<Face> Faces { get; set; }
+
+    }
+}
